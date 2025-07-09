21a3818f3e803ac32703be7f1abf98ab89e93dd0
4rthuzinho <arthurdainova@gmail.com>
2025-07-09 16:08:56 -0300

    corrige event + add log dos inputs

--- script.js
+++ script.js
@@ -5,6 +5,13 @@ const askButton = document.getElementById('askButton');
 const aiResponse = document.getElementById('aiResponse');
 const form = document.getElementById('form');
 
-form.addEventListener('submit',() => {
-    preventDefault();
+form.addEventListener('submit',(event) => {
+    event.preventDefault();
+
+    const inputs = [
+        apiKeyInput.value,
+        gameSelect.value,
+        questionInput.value
+    ]
+    console.log("inputs:", inputs)
 })
