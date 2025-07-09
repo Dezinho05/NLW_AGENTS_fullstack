f0c61e10e4307d475fa853e20cacdb7a20bca5c0
4rthuzinho <arthurdainova@gmail.com>
2025-07-09 14:49:07 -0300

    mostrando o resultado do form na tela e limpando dados do input

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-const apiKeyInput = document.getElementById('apikey');
-const gameSelect = document.getElementById('gameSelect');
-const questionInput = document.getElementById('questionInput');
-const askButton = document.getElementById('askButton');
-const aiResponse = document.getElementById('aiResponse');
-const form = document.getElementById('form');
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
 
-form.addEventListener('submit',() => {
-    preventDefault();
-})
+    const apiKey = apikey.value;
+    const game = gameSelect.value;
+    const question = questionInput.value;
+
+    aiResponse.innerHTML = `
+        <p><strong>API Key:</strong> ${apiKey}</p>
+        <p><strong>Jogo:</strong> ${game}</p>
+        <p><strong>Pergunta:</strong> ${question}</p>
+    `;
+
+    apikey.value = '';
+    gameSelect.value = '';
+    questionInput.value = '';
+});
